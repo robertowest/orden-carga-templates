d8b427de031cbd355671dffa62f0f4d27e7bfa82
robertowest <roberto.west@gmail.com>
2020-03-15 21:46:03 -0300

    correccion

--- config/settings.py
+++ config/settings.py
@@ -126,27 +126,6 @@ INSTALLED_APPS += [
     'apps.app2',
 ]
 
-TEMPLATES = [{
-    'BACKEND': 'django.template.backends.django.DjangoTemplates',
-    'DIRS': [],  # os.path.join(BASE_DIR, 'templates')
-    'OPTIONS': {
-        'context_processors': [
-            'django.template.context_processors.debug',
-            'django.template.context_processors.request',
-            'django.contrib.auth.context_processors.auth',
-            'django.contrib.messages.context_processors.messages',
-        ],
-        'loaders': [
-            ('django.template.loaders.cached.Loader', [
-                # 'django.template.loaders.filesystem.Loader',
-                'django.template.loaders.app_directories.Loader',
-                'config.loaders.Loader',
-            ]),
-        ],
-    },
-}]
- 
-
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
